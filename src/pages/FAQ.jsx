0f37ea0f3e2266b948f89af064ed996f829e47cf
Omar <omario@sas.upenn.edu>
2022-11-13 10:43:45 -0500

    Added FAQ

--- src/pages/FAQ.jsx
+++ src/pages/FAQ.jsx
@@ -9,28 +9,29 @@ const FAQ = () => {
         [
             {   key: "0",
                 show: "show",
-                title: 'What is an NFT?',
-                text: 'NFTs or non-fungible tokens, are cryptographic assets on blockchain with unique identification codes and metadata that distinguish them from each other. NFTs are unique and not mutually interchangeable, which means no two NFTs are the same.'
+                title: 'What is a carbon credit?',
+                text: 'A carbon credit is a transferable certificate that represents reduction or removal of one metric tonne of carbon dioxide or equivalent.'
             },
             {
                 key: "1",
-                title: 'Customer support is available ?',
-                text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod.'
+                title: 'What is a GBM Auction?',
+                text: 'A GBM auction is a time-limited and incentivized open ascending auction. Bids must be fully funded i.e. bidders commit the full amount when placing their bid. Each new bid needs to be higher than the previous bid by a minimum step, calculated as a percentage of the previous bid. When someone is outbid, they are instantly refunded their bid, as well as an incentive. This incentive is calculated as a percentage of their bid amount, and this percentage depends on the ratio between the previous bid and their bid. The auction runs for a set time, and bids placed just before the end of the auction extend the duration of the auction (which is the technological equivalent of saying “Going once, going twice, sold!” when a new bid is placed at the end of an auction).\n' +
+                    'The highest bidder at the end of a GBM auction wins the carbon credit and their winning bid is the final price paid for the credit, just like in a traditional English auction. The revenue of the GBM auction (i.e. the money collected by the project provider and our platform [the auction house]) is equal to the winning bid, minus the sum of the incentives that were distributed to bidders during the auction.\n'
             },
             {
                 key: "2",
-                title: 'How do I find my transaction hash?',
-                text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod.'
+                title: 'Why does a GBM auction make sense for a Carbon Credits marketplace?',
+                text: "Axies’ GBM auction removes another barrier for companies looking to enter the carbon offset market by neutralizing the costs a business may incur while looking for carbon offsets (through our auction’s incentives). Businesses now have no reasons not to participate in the carbon offset market."
             },
             {
                 key: "3",
-                title: 'What are gas fees on Axies?',
-                text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod.'
-            },
-            {
-                key: "4",
-                title: 'What is the effective staking amount?',
-                text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod.'
+                title: 'What projects does Axies support?',
+                text: "Axies sources projects which share our core values. Axies supports a variety of sustainable projects from around the world ranging from:\n" +
+                    "Forestry and land use\n" +
+                    "Renewable energy\n" +
+                    "Waste management\n" +
+                    "Chemical processes\n" +
+                    "Industrial manufacturing\n"
             },
         ]
     )
